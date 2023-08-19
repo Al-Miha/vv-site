7569e19bd767601ddce84ca3c50978540659c0ef
Al Miha <contact.almiha@gmail.com>
2023-08-19 08:28:57 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Add custom JavaScript here
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
